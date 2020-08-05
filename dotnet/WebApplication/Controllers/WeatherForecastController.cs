5d65fe7f065d4a9448a88ea388843f951323f1bd
max <public.max@outlook.com>
2020-08-04 23:09:53 -0700

    add dotnet

---
+++ dotnet/WebApplication/Controllers/WeatherForecastController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication.Controllers
+{
+    [ApiController]
+    [Route("/api/netcore")]
+    public class WeatherForecastController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "hello fellas";
+        }
+    }
+}
